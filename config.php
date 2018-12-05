3b0830184e3eabfaa8460c88a55a601257ec4e5a
tanjing1019 <37397120+tanjing1019@users.noreply.github.com>
2018-12-05 16:54:31 +0800

    修改为类的写法
    Signed-off-by: tanjing1019 <37397120+tanjing1019@users.noreply.github.com>

---
+++ config.php
@@ -0,0 +1,46 @@
+<?php
+// $dsn = 'mysql:dbname=moodle_xiaoli;host=218.199.144.142';
+// $user = 'moodle_xiaoli';//sqlmoodle
+// $password = 'EKzn2LB4NfCSNwbp';//wsyumoodle
+// $obj = new stdClass();
+// try {
+//     $dbh = new PDO($dsn, $user, $password);
+//     // echo "连接成功";
+// } catch (PDOException $e) {
+//     $obj->getMessage = '1001' ;//链接远程数据库失败
+//     echo json_encode($obj);
+//     exit;
+// }
+/**
+ * 链接服务器配置
+ */
+class database_config 
+{
+	public $getMessage = '';
+	// public $dbh;
+	//数据库配置信息  start
+	private $dsn = 'mysql:dbname=moodle_xiaoli;host=218.199.144.142';
+	private $user = 'moodle_xiaoli';//sqlmoodle
+	private $password = 'EKzn2LB4NfCSNwbp';//wsyumoodle
+	//数据库配置信息  end
+	public function __construct()
+	{
+		global $db;
+		try {
+		    $db = new PDO($this->dsn, $this->user, $this->password);
+		    return $this->getMessage = '1000' ;//链接远程数据库成功
+		    //return true;
+		} catch (PDOException $e) {
+		    return $this->getMessage = '1001' ;//链接远程数据库失败
+		    //print "连接失败le \n";
+		    // return false;
+		}
+	}
+}
+//数据库配置信息  start
+// $dsn = 'mysql:dbname=moodle_xiaoli;host=218.199.144.142';
+// $user = 'moodle_xiaoli';//sqlmoodle
+// $password = 'EKzn2LB4NfCSNwbp';//wsyumoodle
+// $dbh = new database_config();
+//数据库配置信息  end
+?> 
--- teaching_calendar.php
+++ teaching_calendar.php
@@ -1,38 +1,80 @@
 <?php
 header('content-type:application/json;charset=utf-8');
-$dsn = 'mysql:dbname=moodle_xiaoli;host=218.199.144.142';
-$user = 'moodle_xiaoli';//sqlmoodle
-$password = 'EKzn2LB4NfCSNwbp';//wsyumoodle
-$obj = new stdClass();
-try {
-    $dbh = new PDO($dsn, $user, $password);
-    // echo "连接成功";
-} catch (PDOException $e) {
-    $obj->getMessage = '1001' ;//链接远程数据库失败
-    echo json_encode($obj);
-    exit;
+include_once "config.php"; 
+// $dbh = new database_config();
+// //定义空函数  start
+// $obj = new stdClass();
+// //定义空函数  end
+// $obj->getMessage = $dbh->getMessage;
+// echo json_encode($obj);
+/**
+ * 链接XNXQ数据表 和 链接XLRQ数据表
+ */
+class table_data extends database_config
+{
+	//private $dbh;
+	function __construct()
+	{
+		global $db;
+		$dbh = parent::__construct();
+		//print_r($this->dbh);
+		//定义空函数  start
+		$obj = new stdClass();
+		//定义空函数  end
+		$obj->getMessage = $dbh;
+		// print_r(json_encode($obj)) ;
+		if($obj->getMessage=='1000') {
+			if(isset($_GET['table_name'])) {
+				if($_GET['table_name'] == 'XNXQ') {
+					$sql = 'SELECT * FROM moodle_xiaoli.XNXQ  where  CURRENT_DATE() BETWEEN KXRQ AND FJRQ ORDER BY FJRQ ASC;';
+					$obj->getMessage = '1002' ;//链接XNXQ数据表成功！
+					$obj->data = array();
+					$sth = $db->prepare($sql);
+					$sth->execute();
+					$result = $sth->fetchAll(PDO::FETCH_CLASS);
+					$obj->data[] = $result;
+					$json=json_encode($obj);
+					echo $json;
+				} else if($_GET['table_name'] == 'XLRQ') {
+					$sql = 'SELECT * FROM moodle_xiaoli.XLRQ where  CURRENT_DATE() < JSSJ ORDER BY JSSJ ASC;';
+					$obj->getMessage = '1003' ;//链接XLRQ数据表成功！
+					$obj->data = array();
+					$sth = $dbh->prepare($sql);
+					$sth->execute();
+					$result = $sth->fetchAll(PDO::FETCH_CLASS);
+					$obj->data[] = $result;
+					$json=json_encode($obj);
+					echo $json;
+				} 
+			}
+		}
+	}
 }
-if(isset($_GET['table_name'])) {
-	if($_GET['table_name'] == 'XNXQ') {
-		$sql = 'SELECT * FROM moodle_xiaoli.XNXQ  where  CURRENT_DATE() BETWEEN KXRQ AND FJRQ ORDER BY FJRQ ASC;';
-		$obj->getMessage = '1002' ;//链接XNXQ数据表成功！
-		$obj->data = array();
-		$sth = $dbh->prepare($sql);
-		$sth->execute();
-		$result = $sth->fetchAll(PDO::FETCH_CLASS);
-		$obj->data[] = $result;
-		$json=json_encode($obj);
-		echo $json;
-	} else if($_GET['table_name'] == 'XLRQ') {
-		$sql = 'SELECT * FROM moodle_xiaoli.XLRQ where  CURRENT_DATE() < JSSJ ORDER BY JSSJ ASC;';
-		$obj->getMessage = '1003' ;//链接XLRQ数据表成功！
-		$obj->data = array();
-		$sth = $dbh->prepare($sql);
-		$sth->execute();
-		$result = $sth->fetchAll(PDO::FETCH_CLASS);
-		$obj->data[] = $result;
-		$json=json_encode($obj);
-		echo $json;
-	} 
-}
+new table_data();
+exit;
+
+
+// if(isset($_GET['table_name'])) {
+// 	if($_GET['table_name'] == 'XNXQ') {
+// 		$sql = 'SELECT * FROM moodle_xiaoli.XNXQ  where  CURRENT_DATE() BETWEEN KXRQ AND FJRQ ORDER BY FJRQ ASC;';
+// 		$obj->getMessage = '1002' ;//链接XNXQ数据表成功！
+// 		$obj->data = array();
+// 		$sth = $dbh->prepare($sql);
+// 		$sth->execute();
+// 		$result = $sth->fetchAll(PDO::FETCH_CLASS);
+// 		$obj->data[] = $result;
+// 		$json=json_encode($obj);
+// 		echo $json;
+// 	} else if($_GET['table_name'] == 'XLRQ') {
+// 		$sql = 'SELECT * FROM moodle_xiaoli.XLRQ where  CURRENT_DATE() < JSSJ ORDER BY JSSJ ASC;';
+// 		$obj->getMessage = '1003' ;//链接XLRQ数据表成功！
+// 		$obj->data = array();
+// 		$sth = $dbh->prepare($sql);
+// 		$sth->execute();
+// 		$result = $sth->fetchAll(PDO::FETCH_CLASS);
+// 		$obj->data[] = $result;
+// 		$json=json_encode($obj);
+// 		echo $json;
+// 	} 
+// }
 ?> 

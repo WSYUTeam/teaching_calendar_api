42e287dfc822a10a66f35023cbe488d8a15d23d9
tanjing1019 <37397120+tanjing1019@users.noreply.github.com>
2018-12-05 14:52:27 +0800

    Update teaching_calendar.php
    教学日历数据接口

---
+++ teaching_calendar.php
@@ -0,0 +1,39 @@
+<?php
+//$dsn = 'mysql:dbname=sqlmoodle;host=127.0.0.1';
+header('content-type:application/json;charset=utf-8');
+$dsn = 'mysql:dbname=moodle_xiaoli;host=218.199.144.142';
+$user = 'moodle_xiaoli';//sqlmoodle
+$password = 'EKzn2LB4NfCSNwbp';//wsyumoodle
+$obj = new stdClass();
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    // echo "连接成功";
+} catch (PDOException $e) {
+    $obj->getMessage = '1001' ;//链接远程数据库失败
+    echo json_encode($obj);
+    exit;
+}
+if(isset($_GET['table_name'])) {
+	if($_GET['table_name'] == 'XNXQ') {
+		$sql = 'SELECT * FROM moodle_xiaoli.XNXQ  where  CURRENT_DATE() BETWEEN KXRQ AND FJRQ ORDER BY FJRQ ASC;';
+		$obj->getMessage = '1002' ;//链接XNXQ数据表成功！
+		$obj->data = array();
+		$sth = $dbh->prepare($sql);
+		$sth->execute();
+		$result = $sth->fetchAll(PDO::FETCH_CLASS);
+		$obj->data[] = $result;
+		$json=json_encode($obj);
+		echo $json;
+	} else if($_GET['table_name'] == 'XLRQ') {
+		$sql = 'SELECT * FROM moodle_xiaoli.XLRQ where  CURRENT_DATE() < JSSJ ORDER BY JSSJ ASC;';
+		$obj->getMessage = '1003' ;//链接XLRQ数据表成功！
+		$obj->data = array();
+		$sth = $dbh->prepare($sql);
+		$sth->execute();
+		$result = $sth->fetchAll(PDO::FETCH_CLASS);
+		$obj->data[] = $result;
+		$json=json_encode($obj);
+		echo $json;
+	} 
+}
+?> 

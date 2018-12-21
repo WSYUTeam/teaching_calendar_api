c8acfb41388132eb05151a06e4d0684f320281bf
tanjing1019 <37397120+tanjing1019@users.noreply.github.com>
2018-12-21 08:46:01 +0800

    优化代码
    Signed-off-by: tanjing1019 <37397120+tanjing1019@users.noreply.github.com>

--- teaching_calendar_view.php
+++ teaching_calendar_view.php
@@ -1,4 +1,6 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT']."/moodle/teaching_calendar_config.php"; 
+$dbh = new database_config();
 if(!isset($_GET['year'])) {
 	$_GET['year'] = date('Y');
 }
@@ -11,54 +13,27 @@ if(!isset($_GET['month'])) {
 	<input type="text" value="<?php echo $_GET['month'];?>" name="month"> 月
 	<input type="submit">
 </form>
-<?php
-//$dsn = 'mysql:dbname=sqlmoodle;host=127.0.0.1';
-$dsn = 'mysql:dbname=moodle_xiaoli;host=218.199.144.142';
-$user = 'moodle_xiaoli';//sqlmoodle
-$password = 'EKzn2LB4NfCSNwbp';//wsyumoodle
 
-try {
-    $dbh = new PDO($dsn, $user, $password);
-    // echo "连接成功";
-    ?>
-    <script type="text/javascript" src="jquery-1.10.2.min.js"></script>
-	<body>
-		<!-- XNXQ:<br> -->
-		status:
-		<div id="status">
-		</div>
-		weekly:
-		<div id="weekly">
-		</div>
-		<!-- XLRQ:<br>
-		<div id="data_XLRQ">
-		</div> -->
-	</body>
-    <script>
-    $(document).ready(function() {
-	    $.get("teaching_calendar.php", { year:"<?php echo $_GET['year'];?>", month:"<?php echo $_GET['month'];?>" } , function(data){
-	    	// alert(data['status']);
-		  $("#status").html(data['status']);
-			//alert(data['weekly'].length);
-			// $weekly_html = '';
-			for($weekly_i=0; $weekly_i<data['weekly'].length; $weekly_i++) {
-				// $weekly_html += "<div>"+data['weekly'][$weekly_i]+"</div>";
-				//日历获取对应值
-				$(".teaching_calendar:eq("+$weekly_i+")").text(data['weekly'][$weekly_i]);
-				// alert($weekly_html);
-			}	
-		  // $("#weekly").html($weekly_html);
-		},"json");
-		// $.get("teaching_calendar.php?XLRQ=1", function(data){
-		//   $("#data_XLRQ").html(data);
-		// });
-	})
-	</script>
-	<?php
-} catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
-}
-?> 
+<script type="text/javascript" src="jquery-1.10.2.min.js"></script>
+<body>
+	<!-- XNXQ:<br> -->
+	status:
+	<div id="status">
+	</div>
+</body>
+<script>
+$(document).ready(function() {
+    $.get("teaching_calendar.php", { year:"<?php echo $_GET['year'];?>", month:"<?php echo $_GET['month'];?>" } , function(data){
+    	// alert(data['status']);
+	  $("#status").html(data['status']);
+		//alert(data['weekly'].length);
+		for($weekly_i=0; $weekly_i<data['weekly'].length; $weekly_i++) {
+			//日历获取对应值
+			$(".teaching_calendar:eq("+$weekly_i+")").text(data['weekly'][$weekly_i]);
+		}	
+	},"json");
+})
+</script>
 <table class="minicalendar calendartable">
             <caption class="calendar-controls">
                     <h3>
